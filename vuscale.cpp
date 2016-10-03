c8ebbe40f184f740e3bfa5506742bec4a1e707f9
heatblazer <ilianzapryanov@abv.bg>
2016-10-03 21:29:47 +0300

    vuscale: added ruler

--- vuscale.cpp
+++ vuscale.cpp
@@ -25,7 +25,7 @@ void VuScale::paintEvent(QPaintEvent *ev)
     _drawBaseLine(QPoint(m_width, 0), QPoint(m_width, m_height), QColor(255,0,0), &pnt, &path);
 
 
-    int step = (m_width / 10);
+    int step = (m_height / 20);
     _drawRuler(step, QColor(255,0,0), &pnt, &path);
 
 
@@ -41,14 +41,17 @@ void VuScale::_drawBaseLine(QPoint x1, QPoint x2, QColor color, QPainter * const
 
 void VuScale::_drawRuler(int step, QColor color, QPainter * const pnt, QPainterPath *path)
 {
-    path->moveTo(0 ,0);
-    for(int i=0; i < m_height; i += step) {
+    path->moveTo(m_width ,0);
+    int j = 1;
+    int i;
+    for(i=0; i < m_height; i+= step, j++) {
         pnt->setPen(color);
-        if (i % 2 == 0) {
+        if (j % 2 == 0) {
             pnt->drawLine(QPoint(m_width, i), QPoint(m_width-(m_width/2), i));
         } else {
             pnt->drawLine(QPoint(m_width, i), QPoint(0, i));
         }
     }
+    pnt->drawLine(QPoint(m_width, m_height), QPoint(0, m_height));
 
 }
